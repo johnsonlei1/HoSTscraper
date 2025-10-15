fca9636376f8c400ce5d2e49d186efb5de050cff
johnsonlei1 <johnson.lei05@gmail.com>
2025-10-15 15:54:25 -0400

    Initial commit

--- searchresults.py
+++ searchresults.py
@@ -36,7 +36,7 @@ def scrape(url):
     return e.extract(r.text)
 
 # product_data = []
-with open("search_results_urls.txt",'r') as urllist, open('search_results_output.jsonl','w') as outfile, open('urls.txt','a') as product_urls:
+with open("search_results_urls.txt",'r') as urllist, open('search_results_output.jsonl','w') as outfile, open('urls.txt','w') as product_urls:
     for url in urllist.read().splitlines():
         data = scrape(url) 
         if data:
